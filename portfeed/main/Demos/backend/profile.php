add9a38f14cdf79fb223f3ef60a536373b5012b5
Alexander Jefferies <Alex@Alexanders-MacBook-Pro.local>
2014-07-10 19:19:07 +0100

    profile menu bar

--- portfeed/main/Demos/backend/profile.php
+++ portfeed/main/Demos/backend/profile.php
@@ -4,6 +4,17 @@
 
 <br>
 
+<head>
+    	<title>Port Feed</title>
+    	<script type="text/javascript" language="javascript" src="lytebox.js"></script>
+    	<link rel="stylesheet" href="lytebox.css" type="text/css" media="screen" />
+        
+        <style type="text/css">
+			body { font-family: Verdana, Geneva, sans-serif; font-size: 18px; }
+			a:link, a:visited { color: #666666; font-weight: normal; margin: 1px; }
+		</style>
+    </head>
+
 <div class="tabs-left">
 
 	<ul class="nav nav-tabs">
@@ -22,6 +33,52 @@
 		<?php endif; ?>
 
 	</ul>
+	
+	<div class="menu" class="nosel" style=" z-index: 999999; width:150px; height:100%; position:fixed; left:0%; top: 0%; background:black;   ">
+
+<div class="uinfo" style=" display: none; position:fixed; z-index: 1;">
+
+<p style="postition: fixed; width;30px; height:10px; left: 50%; font-size: 20px; ">
+ 
+</p>
+</div>
+<a href="profile.php">
+<div class="avatar" style=" ">
+<img src="images/avatar.svg" width="70" height="70" style="position: absolute; top: 25px; margin: 38 38; left:27%; z-index: 999999; "   />
+<p style="position:relative; top:33%; font-family:futura; font-size: 35px; left: 53%; ">
+   <?php echo $_SESSION['jigowatt']['username']; ?>
+
+
+
+       
+
+</div>
+</a>
+<a href="feed.html">
+<div  class="icon">
+
+
+<img src="images/feed.svg" width="70" height="70" style="position: absolute; top: 150px; margin: 38 38; left:27%; " href="feed.html" />
+
+</div>
+</a>
+<div class="icon">
+<a style="width: 100%; height: 130px; position:absolute; z-index: 999999;  " href="javascript:;" onClick="document.getElementById('hideaway').style.display='block'; "> </a>
+<img src="images/search.svg" width="70" height="70" style="position: absolute; top: 280px; margin: 38 38; left:27%; "  />
+</div>
+
+<a href="myprojects.html">
+<div class="icon">
+<img src="images/projects.svg" width="70" height="70" style="position: absolute; top: 410px; margin: 38 38; left:27%; "  />
+</div>
+</a>
+
+<a href="logout.php">
+<div>
+<img href="logout.php" src="images/off.svg" width="30" height="30" style="position: absolute; top: 90%; margin: 38 38; left:40%; "  />
+
+</div>
+</a>
 
 	<form class="form-horizontal" method="post" action="profile.php">
 	<div class="tab-content">
@@ -121,6 +178,10 @@
 	</div>
 	</form>
 
+<a href="uploadFile.php" class="lytebox" data-lyte-options="width:600 height:340" data-title="Image Upload">Upload Image</a><br /><br />
+
+		<!-- Once our image upload completes, the below link will be shown and the "href" attribute updated with the path to the image //-->
+		<a href="" class="lytebox" style="display: none;" id="uploadedFile" />Click Here to View</a>
 
 </div>
 
